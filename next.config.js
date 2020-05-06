1b052406e96210e7c9c933cb238197c16a9ff6ac
Max Zueff <maxx.zueff@gmail.com>
2020-05-06 12:52:05 +0300

    finish

---
+++ next.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+	env: {
+		// MONGO_URI: "mongodb+srv://max:Qwerty123@cluster0-k12zr.mongodb.net/test?retryWrites=true&w=majority"
+		MONGO_URI: "mongodb://localhost/test"
+	}
+}
--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,6 +1,10 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default (req, res) => {
+import dbConnect from '../../utils/dbConnect'
+
+dbConnect();
+
+export default async (req, res) => {
   res.statusCode = 200
-  res.json({ name: 'John Doe' })
+  res.json({ test: 'test' })
 }
